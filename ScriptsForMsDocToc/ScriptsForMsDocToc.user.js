75fef23e19a0a143700f20842a84c4d5f595ce67
fish <fish404hsif@gmail.com>
2022-05-10 13:46:21 +0800

    Update and rename ScriptsForMsDocToc.js to ScriptsForMsDocToc.user.js

--- ScriptsForMsDocToc/ScriptsForMsDocToc.user.js
+++ ScriptsForMsDocToc/ScriptsForMsDocToc.user.js
@@ -5,8 +5,8 @@
 // @description  Sticky Microsoft Documents right sidebar TOC block
 // @description:zh 固定微软文档右侧的目录栏
 // @author       Fish__404
-// @updateURL    https://raw.githubusercontent.com/fish-404/UserScripts/main/ScriptsForMsDocToc/ScriptsForMsDocToc.js
-// @downloadURL  https://raw.githubusercontent.com/fish-404/UserScripts/main/ScriptsForMsDocToc/ScriptsForMsDocToc.js
+// @updateURL    https://raw.githubusercontent.com/fish-404/UserScripts/main/ScriptsForMsDocToc/ScriptsForMsDocToc.meta.js
+// @downloadURL  https://raw.githubusercontent.com/fish-404/UserScripts/main/ScriptsForMsDocToc/ScriptsForMsDocToc.user.js
 // @match        *://docs.microsoft.com/*
 // @grant        none
 // ==/UserScript==
