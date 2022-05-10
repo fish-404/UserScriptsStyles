017966b69ded1d3cb66790cdd1f79be689d7fb23
fish <fish404hsif@gmail.com>
2022-05-10 13:46:45 +0800

    Create ScriptsForMsDocToc.meta.js

---
+++ ScriptsForMsDocToc/ScriptsForMsDocToc.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         ScriptsForMsDocToc
+// @namespace    mailto:fish404hsif@gmail.com
+// @version      0.3
+// @description  Sticky Microsoft Documents right sidebar TOC block
+// @description:zh 固定微软文档右侧的目录栏
+// @author       Fish__404
+// @updateURL    https://raw.githubusercontent.com/fish-404/UserScripts/main/ScriptsForMsDocToc/ScriptsForMsDocToc.meta.js
+// @downloadURL  https://raw.githubusercontent.com/fish-404/UserScripts/main/ScriptsForMsDocToc/ScriptsForMsDocToc.user.js
+// @match        *://docs.microsoft.com/*
+// @grant        none
+// ==/UserScript==
