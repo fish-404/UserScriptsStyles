150ffa82af94adf4d94659ac6c8985334e382337
fish <fish404hsif@gmail.com>
2022-05-12 08:47:10 +0800

    feat: click show more default

--- MsDocToc/ScriptsForMsDocToc.user.js
+++ MsDocToc/ScriptsForMsDocToc.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         ScriptsForMsDocToc
 // @namespace    mailto:fish404hsif@gmail.com
-// @version      0.3.1
+// @version      0.4.0
 // @description  Sticky Microsoft Documents right sidebar TOC block
 // @description:zh 固定微软文档右侧的目录栏
-// @author       Fish__404
+// @author       fish-404
 // @updateURL    https://raw.githubusercontent.com/fish-404/UserScripts/main/ScriptsForMsDocToc/ScriptsForMsDocToc.meta.js
 // @downloadURL  https://raw.githubusercontent.com/fish-404/UserScripts/main/ScriptsForMsDocToc/ScriptsForMsDocToc.user.js
 // @match        *://docs.microsoft.com/*
@@ -15,7 +15,8 @@
     'use strict';
     try { 
       document.getElementById("affixed-right-container").setAttribute("style", "position: sticky; top: 3vh");
-      document.getElementById("right-rail-in-this-article-list").setAttribute("style", "overflow: auto; max-height: 80vh;");
+      document.getElementById("right-rail-in-this-article-list").setAttribute("style", "overflow: auto; max-height: 70vh;");
+      document.querySelector("button[data-bi-name='show-more-btn']").click();
     }
     catch {} // not all page has right sidebar TOC block
 })();
