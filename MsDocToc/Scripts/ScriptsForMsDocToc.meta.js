6fd0b2ecd3b527eeb9b0511cf2875f48e1d72cf2
fish <fish404hsif@gmail.com>
2022-05-14 18:30:03 +0800

    Update ScriptsForMsDocToc.meta.js

--- MsDocToc/Scripts/ScriptsForMsDocToc.meta.js
+++ MsDocToc/Scripts/ScriptsForMsDocToc.meta.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         ScriptsForMsDocToc
 // @namespace    mailto:fish404hsif@gmail.com
-// @version      0.4.0
+// @version      0.5.0
 // @description  Sticky Microsoft Documents right sidebar TOC block
 // @description:zh 固定微软文档右侧的目录栏
 // @author       Fish__404
-// @updateURL    https://raw.githubusercontent.com/fish-404/UserScriptsStyles/main/MsDocToc/ScriptsForMsDocToc.meta.js
-// @downloadURL  https://raw.githubusercontent.com/fish-404/UserScriptsStyles/main/MsDocToc/ScriptsForMsDocToc.user.js
+// @updateURL    https://raw.githubusercontent.com/fish-404/UserScriptsStyles/main/MsDocToc/Scripts/ScriptsForMsDocToc.meta.js
+// @downloadURL  https://raw.githubusercontent.com/fish-404/UserScriptsStyles/main/MsDocToc/Scripts/ScriptsForMsDocToc.user.js
 // @match        *://docs.microsoft.com/*
 // @grant        none
 // ==/UserScript==
