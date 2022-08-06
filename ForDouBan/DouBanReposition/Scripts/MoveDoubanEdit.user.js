fd5dc6c2ef6d6bff10b09ac0568a9f85a921fef9
fish <fish404hsif@gmail.com>
2022-08-06 16:48:09 +0800

    feat: add new script to move douban edit for review and notes

---
+++ ForDouBan/DouBanReposition/Scripts/MoveDoubanEdit.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         MoveDoubanEdit
+// @namespace    mailto:fish404hsif@gmail.com
+// @version      0.1.0
+// @license      MIT
+// @description  move and edit button to the top and sticky
+// @description:zh 编辑按钮至于顶部且滚动时固定
+// @author       Fish__404
+// @match        https://book.douban.com/review/*
+// @match        https://movie.douban.com/review/*
+// @match        https://music.douban.com/review/*
+// @match        https://www.douban.com/review/*
+// @match        https://www.douban.com/note/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=douban.com
+// @grant        none
+// ==/UserScript==
