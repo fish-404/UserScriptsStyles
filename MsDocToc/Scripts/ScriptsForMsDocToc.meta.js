a6ba9eddd77d6867b407a993b2d5c111b9dfcac3
fish <fish404hsif@gmail.com>
2022-09-17 01:04:12 +0000

    chore: add match rule

--- MsDocToc/Scripts/ScriptsForMsDocToc.meta.js
+++ MsDocToc/Scripts/ScriptsForMsDocToc.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ScriptsForMsDocToc
 // @namespace    mailto:fish404hsif@gmail.com
-// @version      0.6.0
+// @version      0.6.1
 // @license      MIT
 // @description  MSDN TOC Enhance
 // @description:zh 微软文档目录功能新增
@@ -9,5 +9,6 @@
 // @updateURL    https://raw.githubusercontent.com/fish-404/UserScriptsStyles/main/MsDocToc/Scripts/ScriptsForMsDocToc.meta.js
 // @downloadURL  https://raw.githubusercontent.com/fish-404/UserScriptsStyles/main/MsDocToc/Scripts/ScriptsForMsDocToc.user.js
 // @match        *://docs.microsoft.com/*
+// @match        *://learn.microsoft.com/*
 // @grant        none
 // ==/UserScript==
