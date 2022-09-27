be9b31a544d8b1ed9f6028f8e4a63382d713e05e
fish <fish404hsif@gmail.com>
2022-09-27 10:29:37 +0800

    Create NpmPackageRepo.meta.js

---
+++ npm/Scripts/NpmPackageRepo.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         NpmPackageRepo
+// @namespace    mailto:fish404hsif@gmail.com
+// @version      0.1.0
+// @description  See if npm package repo is forked from others
+// @author       fish-404
+// @match        https://www.npmjs.com/package/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=npmjs.com
+// @updateURL    https://raw.githubusercontent.com/fish-404/UserScriptsStyles/main/npm/Scripts/NpmPackageRepo.meta.js
+// @downloadURL  https://raw.githubusercontent.com/fish-404/UserScriptsStyles/main/npm/Scripts/NpmPackageRepo.user.js
+// @license      MIT
+// @grant        GM.xmlHttpRequest
+// ==/UserScript==
