329699ac19b8d5070b01ad7c0c7967a7e37567bd
fish <fish404hsif@gmail.com>
2022-09-27 10:29:07 +0800

    chore: rename & add info

--- npm/Scripts/NpmPackageRepo.user.js
+++ npm/Scripts/NpmPackageRepo.user.js
@@ -6,6 +6,8 @@
 // @author       fish-404
 // @match        https://www.npmjs.com/package/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=npmjs.com
+// @updateURL    https://raw.githubusercontent.com/fish-404/UserScriptsStyles/main/npm/Scripts/NpmPackageRepo.meta.js
+// @downloadURL  https://raw.githubusercontent.com/fish-404/UserScriptsStyles/main/npm/Scripts/NpmPackageRepo.user.js
 // @license      MIT
 // @grant        GM.xmlHttpRequest
 // ==/UserScript==
