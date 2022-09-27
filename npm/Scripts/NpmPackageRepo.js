f0c690e0588133ef2eee9dd8647da6a89313d2ae
fish <fish404hsif@gmail.com>
2022-09-27 10:20:49 +0800

    Create NpmPackageRepo.js

---
+++ npm/Scripts/NpmPackageRepo.js
@@ -0,0 +1,57 @@
+// ==UserScript==
+// @name         NpmPackageRepo
+// @namespace    mailto:fish404hsif@gmail.com
+// @version      0.1.0
+// @description  See if npm package repo is forked from others
+// @author       fish-404
+// @match        https://www.npmjs.com/package/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=npmjs.com
+// @license      MIT
+// @grant        GM.xmlHttpRequest
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let link = document.querySelector('#repository-link').innerText.substring(11);
+    let repo = document.querySelector('#repository');
+
+    const notFoundHtml = '<img src="https://badgen.net/badge/status/404/red" alt="Repository Not Found Badge"/>'
+
+    if (link) {
+        GM.xmlHttpRequest( {
+            method: "GET",
+            url: `https://api.github.com/repos/${link}`,
+            headers: {
+                "accept": "application/vnd.github+json"
+            },
+            onload: function(response) {
+                if (response.status = '200') {
+                    if (response.statusText === "Not Found") {
+                        insertBadge(repo, notFoundHtml);
+                    }
+                    else {
+                        let cur = JSON.parse(response.response);
+                        let curForkHtml = `<img src="https://badgen.net/badge/Forks/${cur.forks}/blue" alt="Repository Total Fork Badge"/>`;
+                        if (cur.fork === true) {
+                            insertBadge(repo, curForkHtml);
+                            insertBadge(repo,
+                                `<a href="${cur.parent.html_url}">
+                                    <img src="https://badgen.net/badge/Parent Forks/${cur.parent.forks}/orange" alt="Respository Parent Fork Badge" />
+                                </a>`);
+                        }
+                        else {
+                            insertBadge(repo, curForkHtml);
+                        }
+                    }
+                }
+            }
+        });
+    }
+    else {
+        console.info("Repository link not found");
+    }
+})();
+
+function insertBadge(element, htmlText) {
+   element.insertAdjacentHTML('afterend', htmlText);
+}
