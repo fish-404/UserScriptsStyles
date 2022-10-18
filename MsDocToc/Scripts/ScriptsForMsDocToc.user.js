45ec1510064954e7f4270624331d809d6c78291a
fish <fish404hsif@gmail.com>
2022-10-18 06:58:48 +0000

    chore: add exclude rule
    Add exclude rule for traning part

--- MsDocToc/Scripts/ScriptsForMsDocToc.user.js
+++ MsDocToc/Scripts/ScriptsForMsDocToc.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ScriptsForMsDocToc
 // @namespace    mailto:fish404hsif@gmail.com
-// @version      0.7.1
+// @version      0.7.2
 // @license      MIT
 // @description  MSDN TOC Enhance
 // @description:zh 微软文档目录功能新增
@@ -10,6 +10,7 @@
 // @updateURL    https://raw.githubusercontent.com/fish-404/UserScriptsStyles/main/MsDocToc/Scripts/ScriptsForMsDocToc.meta.js
 // @downloadURL  https://raw.githubusercontent.com/fish-404/UserScriptsStyles/main/MsDocToc/Scripts/ScriptsForMsDocToc.user.js
 // @match        *://learn.microsoft.com/*
+// @exclude      *://learn.microsoft.com/*/training/*
 // @grant        none
 // ==/UserScript==
 
